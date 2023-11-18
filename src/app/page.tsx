9cac78cde362e966c60f8c5b8945f2240bf6c194
Frainer Encarnacion <fraineralex2001@gmail.com>
2023-11-18 14:07:08 -0400

    add: the projects secction to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import About from './ui/sections/about'
 import Experience from './ui/sections/experience'
+import Projects from './ui/sections/projects'
 
 export default function Home () {
   return (
     <>
     <About />
     <Experience />
+    <Projects />
     </>
   )
 }

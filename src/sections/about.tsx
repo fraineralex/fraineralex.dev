294150cb75020f567dd35a48ca054a65dd3ae0eb
Frainer Encarnación <fraineralex2001@gmail.com>
2023-11-24 16:13:22 -0400

    add: my real information to the about page and add some links in the description

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -1,12 +1,6 @@
 import Image from 'next/image'
-import ItemLink from '../components/layout/item-link'
-import {
-  GitHubIcon,
-  InstagramIcon,
-  TwitterIcon,
-  LinkedinIcon
-} from '../components/common/SvgIcons'
 import { FloatingComponents } from '../components/about/floating-components'
+import { BsDownload } from 'react-icons/bs'
 
 export default function About () {
   return (
@@ -15,18 +9,6 @@ export default function About () {
       className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'
       aria-label='About me'
     >
-      <svg
-        stroke='currentColor'
-        fill='currentColor'
-        stroke-width='0'
-        viewBox='0 0 512 512'
-        className='text-cyan-400 absolute -top-[220px] -right-[40px] animate-[levitate_13s_ease_infinite_1s_reverse]'
-        height='50'
-        width='50'
-        xmlns='http://www.w3.org/2000/svg'
-      >
-        <path d='M418.2 177.2c-5.4-1.8-10.8-3.5-16.2-5.1.9-3.7 1.7-7.4 2.5-11.1 12.3-59.6 4.2-107.5-23.1-123.3-26.3-15.1-69.2.6-112.6 38.4-4.3 3.7-8.5 7.6-12.5 11.5-2.7-2.6-5.5-5.2-8.3-7.7-45.5-40.4-91.1-57.4-118.4-41.5-26.2 15.2-34 60.3-23 116.7 1.1 5.6 2.3 11.1 3.7 16.7-6.4 1.8-12.7 3.8-18.6 5.9C38.3 196.2 0 225.4 0 255.6c0 31.2 40.8 62.5 96.3 81.5 4.5 1.5 9 3 13.6 4.3-1.5 6-2.8 11.9-4 18-10.5 55.5-2.3 99.5 23.9 114.6 27 15.6 72.4-.4 116.6-39.1 3.5-3.1 7-6.3 10.5-9.7 4.4 4.3 9 8.4 13.6 12.4 42.8 36.8 85.1 51.7 111.2 36.6 27-15.6 35.8-62.9 24.4-120.5-.9-4.4-1.9-8.9-3-13.5 3.2-.9 6.3-1.9 9.4-2.9 57.7-19.1 99.5-50 99.5-81.7 0-30.3-39.4-59.7-93.8-78.4zM282.9 92.3c37.2-32.4 71.9-45.1 87.7-36 16.9 9.7 23.4 48.9 12.8 100.4-.7 3.4-1.4 6.7-2.3 10-22.2-5-44.7-8.6-67.3-10.6-13-18.6-27.2-36.4-42.6-53.1 3.9-3.7 7.7-7.2 11.7-10.7zM167.2 307.5c5.1 8.7 10.3 17.4 15.8 25.9-15.6-1.7-31.1-4.2-46.4-7.5 4.4-14.4 9.9-29.3 16.3-44.5 4.6 8.8 9.3 17.5 14.3 26.1zm-30.3-120.3c14.4-3.2 29.7-5.8 45.6-7.8-5.3 8.3-10.5 16.8-15.4 25.4-4.9 8.5-9.7 17.2-14.2 26-6.3-14.9-11.6-29.5-16-43.6zm27.4 68.9c6.6-13.8 13.8-27.3 21.4-40.6s15.8-26.2 24.4-38.9c15-1.1 30.3-1.7 45.9-1.7s31 .6 45.9 1.7c8.5 12.6 16.6 25.5 24.3 38.7s14.9 26.7 21.7 40.4c-6.7 13.8-13.9 27.4-21.6 40.8-7.6 13.3-15.7 26.2-24.2 39-14.9 1.1-30.4 1.6-46.1 1.6s-30.9-.5-45.6-1.4c-8.7-12.7-16.9-25.7-24.6-39s-14.8-26.8-21.5-40.6zm180.6 51.2c5.1-8.8 9.9-17.7 14.6-26.7 6.4 14.5 12 29.2 16.9 44.3-15.5 3.5-31.2 6.2-47 8 5.4-8.4 10.5-17 15.5-25.6zm14.4-76.5c-4.7-8.8-9.5-17.6-14.5-26.2-4.9-8.5-10-16.9-15.3-25.2 16.1 2 31.5 4.7 45.9 8-4.6 14.8-10 29.2-16.1 43.4zM256.2 118.3c10.5 11.4 20.4 23.4 29.6 35.8-19.8-.9-39.7-.9-59.5 0 9.8-12.9 19.9-24.9 29.9-35.8zM140.2 57c16.8-9.8 54.1 4.2 93.4 39 2.5 2.2 5 4.6 7.6 7-15.5 16.7-29.8 34.5-42.9 53.1-22.6 2-45 5.5-67.2 10.4-1.3-5.1-2.4-10.3-3.5-15.5-9.4-48.4-3.2-84.9 12.6-94zm-24.5 263.6c-4.2-1.2-8.3-2.5-12.4-3.9-21.3-6.7-45.5-17.3-63-31.2-10.1-7-16.9-17.8-18.8-29.9 0-18.3 31.6-41.7 77.2-57.6 5.7-2 11.5-3.8 17.3-5.5 6.8 21.7 15 43 24.5 63.6-9.6 20.9-17.9 42.5-24.8 64.5zm116.6 98c-16.5 15.1-35.6 27.1-56.4 35.3-11.1 5.3-23.9 5.8-35.3 1.3-15.9-9.2-22.5-44.5-13.5-92 1.1-5.6 2.3-11.2 3.7-16.7 22.4 4.8 45 8.1 67.9 9.8 13.2 18.7 27.7 36.6 43.2 53.4-3.2 3.1-6.4 6.1-9.6 8.9zm24.5-24.3c-10.2-11-20.4-23.2-30.3-36.3 9.6.4 19.5.6 29.5.6 10.3 0 20.4-.2 30.4-.7-9.2 12.7-19.1 24.8-29.6 36.4zm130.7 30c-.9 12.2-6.9 23.6-16.5 31.3-15.9 9.2-49.8-2.8-86.4-34.2-4.2-3.6-8.4-7.5-12.7-11.5 15.3-16.9 29.4-34.8 42.2-53.6 22.9-1.9 45.7-5.4 68.2-10.5 1 4.1 1.9 8.2 2.7 12.2 4.9 21.6 5.7 44.1 2.5 66.3zm18.2-107.5c-2.8.9-5.6 1.8-8.5 2.6-7-21.8-15.6-43.1-25.5-63.8 9.6-20.4 17.7-41.4 24.5-62.9 5.2 1.5 10.2 3.1 15 4.7 46.6 16 79.3 39.8 79.3 58 0 19.6-34.9 44.9-84.8 61.4zm-149.7-15c25.3 0 45.8-20.5 45.8-45.8s-20.5-45.8-45.8-45.8c-25.3 0-45.8 20.5-45.8 45.8s20.5 45.8 45.8 45.8z'></path>
-      </svg>
       <header className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
         <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>
           About
@@ -42,66 +24,92 @@ export default function About () {
             alt='Frainer Encarnacion profile picture'
           />
         </figure>
-        <FloatingComponents />
+        {/* <FloatingComponents /> */}
         <ul
-          className='ml-1 mt-4 mb-4 flex items-center justify-center'
-          aria-label='Social media'
+          className='mt-6 mb-6 flex items-center justify-center gap-3'
+          aria-label='Cotact me'
         >
-          <ItemLink title='Github' href='https://github.com/fraineralex'>
-            <GitHubIcon />
-          </ItemLink>
-
-          <ItemLink
-            title='Linkedin'
-            href='https://www.linkedin.com/in/fraineralex'
+          <a
+            type='button'
+            className='text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-3 text-center'
+            href='mailto:fraineralex2001@gmail.com'
+            target='_blank'
           >
-            <LinkedinIcon />
-          </ItemLink>
+            Say Hello
+          </a>
 
-          <ItemLink title='Instagram' href='https://instagram.com/frainer.alex'>
-            <InstagramIcon />
-          </ItemLink>
-
-          <ItemLink title='Twitter' href='https://twitter.com/fraineralex'>
-            <TwitterIcon />
-          </ItemLink>
-          <ItemLink title='Github' href='https://github.com/fraineralex'>
-            <GitHubIcon />
-          </ItemLink>
-
-          <ItemLink
-            title='Linkedin'
-            href='https://www.linkedin.com/in/fraineralex'
+          <a
+            className='relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800'
+            aria-label='View Full Résumé'
+            href='/resume.pdf'
           >
-            <LinkedinIcon />
-          </ItemLink>
-
-          <ItemLink title='Instagram' href='https://instagram.com/frainer.alex'>
-            <InstagramIcon />
-          </ItemLink>
-
-          <ItemLink title='Twitter' href='https://twitter.com/fraineralex'>
-            <TwitterIcon />
-          </ItemLink>
+            <span className='relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+              <BsDownload className='inline me-1' /> Résumé
+            </span>
+          </a>
         </ul>
         <p className='mb-4'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
-          consequatur, distinctio, doloribus vitae expedita in consequuntur ipsa
-          laudantium labore asperiores quia impedit, sit animi dolores eligendi
-          id voluptates?
+          I’m Frainer, a passionate{' '}
+          <strong className='font-medium text-slate-200'>
+            Full Stack Developer
+          </strong>{' '}
+          with over 3 years of experience, specializing in bringing ideas to
+          life in the vast world of web development. Throughout my journey, I’ve
+          had the privilege of contributing to software development for a{' '}
+          <a
+            className='font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300'
+            href='https://nelmix.com/'
+            target='_blank'
+            rel='noreferrer'
+            aria-label='Nelmix'
+          >
+            software consultancy
+          </a>
+          , a{' '}
+          <a
+            className='font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300'
+            href='https://ippdr.com/'
+            target='_blank'
+            rel='noreferrer'
+            aria-label='International Pack & Paper'
+          >
+            product company
+          </a>
+          , and a{' '}
+          <a
+            className='font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300'
+            href='https://lifterdo.com'
+            target='_blank'
+            rel='noreferrer'
+            aria-label='Lifter'
+          >
+            financial consultancy.
+          </a>
         </p>
         <p className='mb-4'>
-          Doloribus corporis totam nostrum illo quisquam, nisi earum iste! Sunt
-          corrupti in, nostrum sint voluptates autem doloremque! Cum aliquid at
-          tempore odit quidem minima dignissimos molestiae quibusdam non,
-          facilis quo quaerat voluptate esse iure temporibus nostrum a ad vel.
+          Currently, I’m primarily focused on building accessible products and
+          creating exceptional solutions and impactful digital experiences for
+          our clients at{' '}
+          <a
+            className='font-medium text-slate-200 hover:text-teal-300 focus-visible:text-teal-300'
+            href='https://lifterdo.com'
+            target='_blank'
+            rel='noreferrer'
+            aria-label='Lifter'
+          >
+            Lifter.
+          </a>{' '}
+          Outside the professional realm, I immerse myself in exploring new
+          technologies,{' '}
+          <strong className='font-medium text-slate-200'>
+            working on personal projects,{' '}
+          </strong>
+          and{' '}
+          <strong className='font-medium text-slate-200'>
+            offering freelance services.
+          </strong>
         </p>
-        <p className='mb-4'>
-          Dolore rem recusandae necessitatibus ab consequatur dolorum, libero
-          commodi. Excepturi eveniet, accusamus harum officia quod natus in quos
-          voluptates qui adipisci hic sed quisquam voluptate vero inventore ea
-          magnam porro ut voluptas aliquam?
-        </p>
+        <p className='mb-4'></p>
       </article>
     </section>
   )

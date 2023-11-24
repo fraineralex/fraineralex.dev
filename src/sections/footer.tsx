5d502515ab97bbc00be6fda3afcfd932c149b852
Frainer Encarnación <fraineralex2001@gmail.com>
2023-11-24 16:15:02 -0400

    fix: update the text in the footer

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -2,14 +2,14 @@ export default function Footer () {
   return (
     <footer className='max-w-md pb-16 text-sm text-slate-500 sm:pb-0'>
       <p>
-        Loosely designed by{' '}
+        Inspired by the design of{' '}
         <a
           href='https://brittanychiang.com/'
           className='font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300'
           target='_blank'
           rel='noreferrer'
         >
-          Brittany Chiang
+          Brittany Chiang’s website
         </a>{' '}
         and coded in{' '}
         <a

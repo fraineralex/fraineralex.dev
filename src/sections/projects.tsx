c808e1cc4a2fc82c04d6f107cecb6ef401c1afdc
Frainer Encarnación <fraineralex2001@gmail.com>
2023-11-27 23:35:15 -0400

    wip: adding the icons of the technologies used in the projects

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -1,6 +1,17 @@
 import Link from 'next/link'
 import ProjectCard from '../components/project/project-card'
-import { ArrowRigthIcon } from '../components/common/SvgIcons'
+import { FaSquareJs } from 'react-icons/fa6'
+import { BsBootstrapFill } from 'react-icons/bs'
+
+import {
+  ArrowRigthIcon,
+  CSharpIcon,
+  DotNetIcon,
+  NumpyIcon,
+  PythonIcon,
+  SQLServerIcon,
+  ViteIcon
+} from '../components/common/SvgIcons'
 import { FaReact } from 'react-icons/fa'
 import { TbBrandTypescript } from 'react-icons/tb'
 import { SWRIcon } from '../components/common/SvgIcons'
@@ -36,13 +47,24 @@ export default function Projects () {
               <TbBrandTypescript size={30} className='text-blue-500' />
             </li>
             <li className='mr-4'>
-              <Image src='/images/projects/styled-components.ico' width={30} height={30} alt='Styled ' />
+              <Image
+                src='/images/projects/styled-components.ico'
+                width={30}
+                height={30}
+                alt='Styled '
+              />
             </li>
             <li className='mr-4'>
-            <span>SWR</span>
+              <SWRIcon />
+              <span
+                className='mx-2 font-extrabold hidden md:inline select-none'
+                title='SWR: React Hooks for Data Fetching'
+              >
+                SWR
+              </span>
             </li>
             <li className='mr-4'>
-              <FaReact size={30} className='text-cyan-400' />
+              <ViteIcon />
             </li>
           </ProjectCard>
           <ProjectCard
@@ -52,7 +74,33 @@ export default function Projects () {
             deployUrl='https://frainer-todo-app.surge.sh/'
             githubRepositoryUrl='https://github.com/fraineralex/learning-react/blob/main/projects/08-todo-app-typescript'
             imageSrc='todo.png'
-          />
+          >
+            <li className='mr-4'>
+              <FaReact size={30} className='text-cyan-400' />
+            </li>
+            <li className='mr-4'>
+              <TbBrandTypescript size={30} className='text-blue-500' />
+            </li>
+            <li className='mr-4'>
+              <Image
+                src='/images/projects/autoanimate.ico'
+                width={30}
+                height={30}
+                alt='Auto Animate icon'
+              />
+            </li>
+            <li className='mr-4'>
+              <Image
+                src='/images/projects/jsonbin.ico'
+                width={30}
+                height={30}
+                alt='JsonBin icon'
+              />
+            </li>
+            <li className='mr-4'>
+              <ViteIcon />
+            </li>
+          </ProjectCard>
           <ProjectCard
             projectName='Chess AI'
             description='Artificial Intelligence Chess developed with the minimax adversarial search algorithm with alpha beta pruning. It returns the best possible move it has been able to find at the given depth within a specified time interval, at each iteration the algorithm takes into account a series of Heuristics that tell it whether a particular move is good or bad based on the future outcome it might cause.'
@@ -61,7 +109,40 @@ export default function Projects () {
             githubRepositoryUrl='https://github.com/fraineralex/ChessAI'
             /* starsOnGithub={514} */
             imageSrc='chess.png'
-          />
+          >
+            <li className='mr-4'>
+              <PythonIcon />
+            </li>
+            <li className='mr-4'>
+              <NumpyIcon />
+            </li>
+            <li className='mr-4'>
+              <Image
+                src='/images/projects/pygame.ico'
+                width={30}
+                height={30}
+                alt='Pygame Icon'
+              />
+            </li>
+            <li className='mr-4'>
+              <Image
+                src='/images/projects/chess-library.png'
+                width={30}
+                height={30}
+                alt='Chess library icon'
+                className='border-2 border-transparent rounded-sm'
+              />
+            </li>
+            <li className='mr-4'>
+              <SWRIcon className='px-2' />
+              <span
+                className='font-extrabold hidden md:inline tracking-tighter'
+                title='Minimax algorithm'
+              >
+                Minimax
+              </span>
+            </li>
+          </ProjectCard>
           <ProjectCard
             projectName='Real Estate App'
             description='A real estate management web application with distinct roles (agent, client, admin, and developer), each offering specific functionalities within the system. The project utilizes an integrated RESTful API to facilitate seamless interactions. Developed under the ONION architecture in C# ASP .Net Core, it adheres to the SOLID principles for robust and scalable design.'
@@ -69,7 +150,23 @@ export default function Projects () {
             deployUrl='https://github.com/fraineralex/RealEstate-App'
             githubRepositoryUrl='https://github.com/fraineralex/RealEstate-App'
             imageSrc='real-estate.png'
-          />
+          >
+            <li className='mr-4'>
+              <CSharpIcon />
+            </li>
+            <li className='mr-4'>
+              <DotNetIcon className='border-2 border-transparent rounded-md' />
+            </li>
+            <li className='mr-3'>
+              <FaSquareJs size={30} className='text-yellow-js' />
+            </li>
+            <li className='mr-4'>
+              <SQLServerIcon />
+            </li>
+            <li className='mr-4'>
+              <BsBootstrapFill size={30} className='text-violet-700' />
+            </li>
+          </ProjectCard>
         </ol>
         <div className='mt-12'>
           <Link

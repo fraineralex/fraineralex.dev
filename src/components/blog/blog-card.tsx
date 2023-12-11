6dc4dc9da8f7196b949083f7309d7c1acb110a86
Frainer Encarnación <fraineralex2001@gmail.com>
2023-12-11 01:05:52 -0400

    fix: change the color of the card and aply the shark color to the background hover

--- src/components/blog/blog-card.tsx
+++ src/components/blog/blog-card.tsx
@@ -16,12 +16,12 @@ export default function BlogCard ({ title, imageName, date }: Props) {
   return (
     <li className='mb-12'>
       <div className='group relative grid grid-cols-8 gap-4 transition-all sm:items-center sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
-        <span className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></span>
+        <span className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-shark-800/20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></span>
         <Image
           width={200}
           height={48}
           alt={`Image of the blog ${title}`}
-          className='z-10 col-span-2 rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:col-span-2'
+          className='z-10 col-span-2 rounded border-2 border-shark-200/10 transition group-hover:border-shark-200/30 sm:col-span-2'
           src={`/images/blog/${imageName}`}
           style={{ color: 'transparent' }}
         />
@@ -29,7 +29,7 @@ export default function BlogCard ({ title, imageName, date }: Props) {
           <p className='-mt-1 text-sm font-semibold leading-6'>{date}</p>
           <h3 className='-mt-1'>
             <Link
-              className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
+              className='inline-flex items-baseline font-medium leading-tight text-shark-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
               href={`blog/${title}`}
               target='_blank'
               rel='noreferrer'

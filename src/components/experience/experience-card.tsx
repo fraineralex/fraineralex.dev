8a2acf51c36227f2e4414c8bd82285ec724e557e
Frainer Encarnación <fraineralex2001@gmail.com>
2023-12-11 01:03:34 -0400

    fix: the problem with the background hover and aply the shark color

--- src/components/experience/experience-card.tsx
+++ src/components/experience/experience-card.tsx
@@ -18,20 +18,20 @@ export default function ExperienceCard ({
   return (
     <li className='mb-12'>
       <div className='group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
-        <span className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></span>
+        <span className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-shark-800/20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></span>
         <Image
           width={200}
           height={48}
           alt={`Image of the job experience ${title} at ${company.name} of Frainer Encarnación`}
-          className='z-10 rounded group-hover:border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:col-span-2 mt-2 w-20 mx-auto'
+          className='z-10 rounded group-hover:border-2 border-shark-200/10 transition group-hover:border-shark-200/30 sm:col-span-2 mt-2 w-20 mx-auto'
           src={`/images/experience/${imageName}`}
           style={{ color: 'transparent' }}
         />
         <aside className='z-10 sm:col-span-6'>
-          <header className='font-medium leading-snug text-slate-200'>
+          <header className='font-medium leading-snug text-shark-200'>
             <h3>
               <a
-                className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
+                className='inline-flex items-baseline font-medium leading-tight text-shark-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
                 href={company.website}
                 target='_blank'
                 rel='noreferrer'
@@ -49,15 +49,15 @@ export default function ExperienceCard ({
             </h3>
             {otherTitles &&
               otherTitles.map((title, index) => (
-                <h5 className='text-gray-500' aria-hidden='true' key={index}>
+                <h5 className='text-slate-500' aria-hidden='true' key={index}>
                   {title}
                 </h5>
               ))}
-            <h5 className='text-gray-500 mt-1 text-sm' aria-hidden='true'>
+            <h5 className='text-slate-500 mt-1 text-sm' aria-hidden='true'>
               {startDate} — {endDate} · {contract}
             </h5>
             {location && locationType && (
-              <h5 className='text-gray-500 mt-1 text-sm' aria-hidden='true'>
+              <h5 className='text-slate-500 mt-1 text-sm' aria-hidden='true'>
                 {location} · {locationType}
               </h5>
             )}

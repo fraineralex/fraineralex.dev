55a9625532878494b2e100984e38a97674b7e858
Frainer Encarnación <fraineralex2001@gmail.com>
2023-12-11 00:58:55 -0400

    add: two new color to the color palete with their scales

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,14 +8,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-      },
       colors: {
         'yellow-js': '#FFFF00',
-        'green-node': '#026e00'
+        'green-node': '#026e00',
+        'shark': {
+          '50': '#f6f7f9',
+          '100': '#eceff2',
+          '200': '#d5dce2',
+          '300': '#afbcca',
+          '400': '#8499ac',
+          '500': '#657c92',
+          '600': '#506479',
+          '700': '#425262',
+          '800': '#394653',
+          '900': '#333d47',
+          '950': '#232931',
+      },
+      'black-pearl': {
+        '50': '#f3f7fc',
+        '100': '#e7eff7',
+        '200': '#c9dcee',
+        '300': '#99c0e0',
+        '400': '#62a0ce',
+        '500': '#3e84b9',
+        '600': '#2d689c',
+        '700': '#26547e',
+        '800': '#234869',
+        '900': '#213d59',
+        '950': '#0e1925',
+    },
+        
       },
       keyframes: {
         heartbeat: {

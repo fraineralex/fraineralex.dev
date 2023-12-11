c9bbada83d7cab379d59bf15ff591e4b3a6a1fb8
Frainer Encarnación <fraineralex2001@gmail.com>
2023-12-11 16:10:17 -0400

    feat: install and aply the intersection observer to manage the nav links activation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Locale, i18n } from '@/i18n-config'
 import { getDictionary } from '../get-dictionary'
 import { About, Experience, Projects, Blog, Footer, SideNav } from '@/sections'
+import Observer from '@/components/common/intersection-observer'
 
 interface Props {
   params?: { lang: Locale }
@@ -10,16 +11,5 @@ export default async function Home ({ params }: Props) {
   const lang = params?.lang || i18n.defaultLocale
   const dictionary = await getDictionary(lang)
 
-  return (
-    <div className='lg:flex lg:justify-between lg:gap-10' lang={lang}>
-      <SideNav dictionary={dictionary.sidenav} />
-      <main id='content' className='pt-24 lg:w-1/2 lg:py-24 pr-20'>
-        <About dictionary={dictionary.about} />
-        <Experience dictionary={dictionary.experience} />
-        <Projects dictionary={dictionary.projects} />
-        <Blog dictionary={dictionary.writing} />
-        <Footer dictionary={dictionary.footer} />
-      </main>
-    </div>
-  )
+  return <Observer dictionary={dictionary} lang={lang} />
 }

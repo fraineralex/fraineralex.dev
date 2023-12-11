8501a9fc4ebefb469cd6e2c8628f7208f6225906
Frainer Encarnación <fraineralex2001@gmail.com>
2023-12-11 01:00:25 -0400

    fix: the problem with the backgrounf hover and aply the shark color

--- src/components/project/project-card.tsx
+++ src/components/project/project-card.tsx
@@ -14,11 +14,11 @@ export default function ProjectCard ({
   return (
     <li className='mb-12'>
       <div className='group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50'>
-        <span className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></span>
+        <span className='absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-shark-800/20 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg'></span>
         <aside className='z-10 sm:order-2 sm:col-span-6'>
           <h3>
             <a
-              className='inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
+              className='inline-flex items-baseline font-medium leading-tight text-shark-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base'
               href={deployUrl}
               target='_blank'
               rel='noreferrer'
@@ -34,7 +34,7 @@ export default function ProjectCard ({
           <p className='mt-2 text-sm leading-normal'>{description}</p>
           {starsOnGithub >= 16 && (
             <a
-              className='relative mt-2 inline-flex items-center text-sm font-medium text-slate-300 hover:text-teal-300 focus-visible:text-teal-300'
+              className='relative mt-2 inline-flex items-center text-sm font-medium text-shark-300 hover:text-teal-300 focus-visible:text-teal-300'
               href={githubRepositoryUrl}
               target='_blank'
               rel='noreferrer'
@@ -47,7 +47,7 @@ export default function ProjectCard ({
           <ul className='mt-2 flex flex-wrap' aria-label='Related links'>
             <li className='mr-4'>
               <a
-                className='relative mt-2 inline-flex items-center text-sm font-medium text-slate-300 hover:text-teal-300 focus-visible:text-teal-300'
+                className='relative mt-2 inline-flex items-center text-sm font-medium text-shark-300 hover:text-teal-300 focus-visible:text-teal-300'
                 href={githubRepositoryUrl}
                 target='_blank'
                 rel='noreferrer'
@@ -72,7 +72,7 @@ export default function ProjectCard ({
           alt={`Cover image of the project`}
           width={543}
           height={336}
-          className='rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1'
+          className='rounded border-2 border-shark-200/10 transition group-hover:border-shark-200/30 sm:order-1 sm:col-span-2 sm:transhark-y-1'
           src={`/images/projects/${imageSrc}`}
           style={{ color: 'transparent' }}
         />

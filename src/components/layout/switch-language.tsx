72949b7306da04b129396f55d6b157a91a1d597c
Frainer Encarnación <fraineralex2001@gmail.com>
2024-02-03 18:10:25 -0400

    refactor: add a custom select language who don't use mui

--- src/components/layout/switch-language.tsx
+++ src/components/layout/switch-language.tsx
@@ -1,27 +1,55 @@
 'use client'
-
-import { Locale } from '@/i18n-config'
-import { useState } from 'react'
+import { ChevronDown } from 'lucide-react'
 import { usePathname } from 'next/navigation'
+import { useState, useEffect } from 'react'
+import { Locale } from '@/i18n-config'
 import Link from 'next/link'
-import FormControlLabel from '@mui/material/FormControlLabel'
 import { SpainFlag, UsaFlag } from '../common/SvgIcons'
-import { MaterialUISwitch } from './switch'
 
-export default function SwitchLanguage () {
+type langType = {
+  current: {
+    name: string
+    flag: JSX.Element
+  }
+  new: {
+    name: string
+    flag: JSX.Element
+  }
+}
+
+export function SwitchLanguage () {
   const pathname = usePathname()
-  const [lang, setLang] = useState<Locale>(
+  const [currentLocale, setCurrentLocale] = useState<Locale>(
     pathname.startsWith('/es') ? 'es' : 'en'
   )
+  const [lang, setLang] = useState<langType>()
 
-  const handleSetLanguage = () => {
-    const newLang = lang === 'en' ? 'es' : 'en'
-    setLang(newLang)
-  }
+  useEffect(() => {
+    setLang({
+      current: {
+        name: currentLocale === 'es' ? 'Español' : 'English',
+        flag:
+          currentLocale === 'es' ? (
+            <SpainFlag className='w-4 h-auto' />
+          ) : (
+            <UsaFlag className='w-4 h-auto' />
+          )
+      },
+      new: {
+        name: currentLocale === 'es' ? 'English' : 'Español',
+        flag:
+          currentLocale === 'es' ? (
+            <UsaFlag className='w-4 h-auto' />
+          ) : (
+            <SpainFlag className='w-4 h-auto' />
+          )
+      }
+    })
+  }, [pathname])
 
   let newpath = `/es${pathname}`
 
-  if (lang === 'es') {
+  if (currentLocale === 'es') {
     newpath = pathname.replace('/es', '/')
   }
 
@@ -29,22 +57,31 @@ export default function SwitchLanguage () {
     newpath = pathname.replace('/es/', '/')
   }
 
-  const ariaLabel =
-    lang === 'en' ? 'Traducir a español' : 'Translate to english'
-
   return (
-    <header className='absolute md:fixed right-4 md:right-10 top-1 md:top-5 z-50'>
-      <SpainFlag className='inline' />
-      <Link href={newpath}>
-        <FormControlLabel
-          control={<MaterialUISwitch sx={{ m: 1 }} checked={lang === 'en'} />}
-          label=''
-          className='inline pl-2'
-          aria-label={ariaLabel}
-          onClick={handleSetLanguage}
-        />
-      </Link>
-      <UsaFlag className='inline -ml-5' />
-    </header>
+    <div className='relative -mt-1 -mb-3 text-left duration-200'>
+      <div className='group rounded-md text-xs font-semibold bg-black/30 hover:bg-black/70 transition-all'>
+        <button
+          type='button'
+          className='inline-flex justify-start items-center w-full gap-x-2 px-3 py-2 font-semibold'
+          aria-expanded='true'
+          aria-haspopup='true'
+        >
+          {lang?.current.flag}
+          {lang?.current.name}
+          <ChevronDown className='-mr-1 h-5 w-5' />
+        </button>
+        <ul className='group-hover:block group-hover:animate-fade-down group-hover:animate-duration-200 hidden pt-0.5 absolute w-full'>
+          <li className='py-[2px]'>
+            <Link
+              className='rounded-md bg-black/30 hover:bg-black/70 whitespace-no-wrap inline-flex justify-start items-center w-full gap-x-2 px-3 py-2'
+              href={newpath}
+            >
+              {lang?.new.flag}
+              {lang?.new.name}
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }

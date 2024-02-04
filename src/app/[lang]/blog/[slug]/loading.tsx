cde8218fb9b86adf6d255fcd42c4e1c5bccb9fb8
Frainer Encarnación <fraineralex2001@gmail.com>
2024-02-04 15:47:43 -0400

    fix: remove the navbar from the loading skeleton

--- src/app/[lang]/blog/[slug]/loading.tsx
+++ src/app/[lang]/blog/[slug]/loading.tsx
@@ -26,7 +26,7 @@ export default function Loading () {
                 <path d='M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z' />
               </svg>
               <div>
-                <div className='w-34 h-4 bg-gray-200 rounded-full dark:bg-gray-700'></div>
+                <div className='w-34 h-4 bg-gray-200 rounded-full dark:bg-gray-700'/>
               </div>
             </div>
           </div>
@@ -36,9 +36,9 @@ export default function Loading () {
       <header className='mx-auto text-center mb-8 w-full'>
         <div className='flex items-center justify-center min-h-screen p-5 min-w-screen'>
           <div className='flex space-x-4 animate-pulse'>
-            <div className='w-10 h-10 bg-gradient-to-tr from-indigo-500 to-teal-300 rounded-full '></div>
-            <div className='w-10 h-10 bg-gradient-to-tr from-indigo-500 to-teal-300 rounded-full '></div>
-            <div className='w-10 h-10 bg-gradient-to-tr from-indigo-500 to-teal-300 rounded-full '></div>
+            <div className='w-10 h-10 bg-gradient-to-tr from-indigo-500 to-teal-300 rounded-full '/>
+            <div className='w-10 h-10 bg-gradient-to-tr from-indigo-500 to-teal-300 rounded-full '/>
+            <div className='w-10 h-10 bg-gradient-to-tr from-indigo-500 to-teal-300 rounded-full '/>
           </div>
         </div>
       </header>

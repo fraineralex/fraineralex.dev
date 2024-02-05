ebc3144a1149914120ba9ba4bf7c3d1309b77eb6
Frainer Encarnación <fraineralex2001@gmail.com>
2024-02-05 14:51:24 -0400

    refactor: separate the modal in a component

--- src/components/blog/search/search-posts.tsx
+++ src/components/blog/search/search-posts.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Search } from 'lucide-react'
 import { SearchResult } from './search-result'
-import { Modal } from './search-modal'
+import { Modal } from '@/components/blog/common/modal'
 import { displayTags } from '@/utils/data'
 import Link from 'next/link'
 
@@ -26,46 +26,48 @@ export function SearchPosts ({ className }: { className?: string }) {
       </button>
 
       <Modal isOpen={open} onClose={() => setOpen(false)}>
-        <div className='relative mb-2 w-full md:mb-4'>
-          <input
-            autoFocus
-            type='text'
-            className='w-full placeholder-slate-500 bg-transparent border outline-none disabled:bg-slate-50 text-white border-slate-500 focus:bg-slate-900/50 focus:border-slate-400 rounded-full px-6 py-3'
-            placeholder='Start typing to search'
-            onChange={e => setSearchTerm(e.target.value)}
-          />
-        </div>
-        {!searchTerm && (
-          <small className='mb-4 text-slate-400 flex flex-col sm:flex-row text-center'>
-            <div className='flex flex-wrap'>
-              {displayTags &&
-                displayTags.map((tag, index) => (
-                  <div key={index}>
-                    <Link
-                      href={`/tags/${tag.name}`}
-                      className='text-teal-300 font-bold underline uppercase underline-offset-4 py-3 px-1 hover:text-pink-400'
-                    >
-                      {tag.label}
-                    </Link>
-                    {index !== (displayTags?.length ?? 0) - 1 && (
-                      <span className='px-1'>•</span>
-                    )}
-                  </div>
-                ))}
-            </div>
-          </small>
-        )}
-        <div className='relative w-full'>
+        <div className='fixed left-0 right-0 top-0 z-50 mx-2 mt-32 max-w-[1200px] flex-col items-center overflow-hidden rounded-2xl p-6 shadow-md border border-slate-700 bg-slate-800 md:mx-8 lg:mx-auto lg:w-3/4 xl:w-2/3 md:max-h-50 max-h-132 flex'>
+          <div className='relative mb-2 w-full md:mb-4'>
+            <input
+              autoFocus
+              type='text'
+              className='w-full placeholder-slate-500 bg-transparent border outline-none disabled:bg-slate-50 text-white border-slate-500 focus:bg-slate-900/50 focus:border-slate-400 rounded-full px-6 py-3'
+              placeholder='Start typing to search'
+              onChange={e => setSearchTerm(e.target.value)}
+            />
+          </div>
           {!searchTerm && (
-            <div className='my-4 flex flex-row items-center justify-center text-slate-300'>
-              <Search className='h-5 w-5 me-2' />
-              <h2 id='radix-:rh:' className='font-normal'>
-                Search articles from this blog
-              </h2>
-            </div>
+            <small className='mb-4 text-slate-400 flex flex-col sm:flex-row text-center'>
+              <div className='flex flex-wrap'>
+                {displayTags &&
+                  displayTags.map((tag, index) => (
+                    <div key={index}>
+                      <Link
+                        href={`/tags/${tag.name}`}
+                        className='text-teal-300 font-bold underline uppercase underline-offset-4 py-3 px-1 hover:text-pink-400'
+                      >
+                        {tag.label}
+                      </Link>
+                      {index !== (displayTags?.length ?? 0) - 1 && (
+                        <span className='px-1'>•</span>
+                      )}
+                    </div>
+                  ))}
+              </div>
+            </small>
           )}
+          <div className='relative w-full'>
+            {!searchTerm && (
+              <div className='my-4 flex flex-row items-center justify-center text-slate-300'>
+                <Search className='h-5 w-5 me-2' />
+                <h2 id='radix-:rh:' className='font-normal'>
+                  Search articles from this blog
+                </h2>
+              </div>
+            )}
+          </div>
+          {searchTerm && <SearchResult searchTerm={searchTerm} />}
         </div>
-        {searchTerm && <SearchResult searchTerm={searchTerm} />}
       </Modal>
     </>
   )

522b4bc8b8e896a6ba228a3b192280b14d77d3ab
Frainer Encarnación <fraineralex2001@gmail.com>
2024-02-05 14:48:03 -0400

    add: the modal component to display a react node as a modal card

---
+++ src/components/blog/common/modal.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react'
+import { createPortal } from 'react-dom'
+
+interface ModalProps {
+  isOpen: boolean
+  onClose: () => void
+  children: ReactNode
+}
+
+export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+  if (!isOpen) {
+    return null
+  }
+
+  return createPortal(
+    <>
+      <div
+        className='fixed inset-0 z-50 backdrop-blur-sm bg-gray-500/20'
+        onClick={onClose}
+      />
+      {children}
+    </>,
+    document.body
+  )
+}

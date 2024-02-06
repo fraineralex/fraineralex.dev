a09e55299335716d3214c0cf2d633fcce2276780
Frainer Encarnación <fraineralex2001@gmail.com>
2024-02-06 15:55:50 -0400

    add: the types to use in the blog translations

---
+++ src/types/blog-types.d.ts
@@ -0,0 +1,12 @@
+export interface PostsByTags {
+  label:      string;
+  newsletter: Newsletter;
+}
+
+export interface Newsletter {
+  placeholder: string;
+  label:       string;
+  description: string;
+}
+
+
